98467b458ec2ace822e4f9f074ecfc9f89d59d27
Kiragu <59251264+CalebKiragu@users.noreply.github.com>
2025-06-13 16:53:39 +0300

    added node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import dotenv from "dotenv";
+// import { NodeGlobalsPolyfillPlugin } from "vite-plugin-node-polyfills";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 dotenv.config(); // Load environment variables
 
 // https://vitejs.dev/config/
@@ -11,13 +13,25 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    nodePolyfills({
+      protocolImports: true,
+      globals: {
+        process: true,
+      },
+    }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      buffer: "buffer",
+      process: "process/browser",
     },
+  },
+  define: {
+    global: "globalThis",
   },
   build: {
     outDir: "dist",
